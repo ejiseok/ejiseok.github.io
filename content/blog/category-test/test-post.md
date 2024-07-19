213c1a87aea91484500b9a4cb77fb5fc17d0d368
ejiseok <jiseoklee0502@gmail.com>
2024-07-19 13:39:18 +0900

    add test posts

---
+++ content/blog/category-test/test-post.md
@@ -0,0 +1,8 @@
+---
+title: test post
+date: 2024-07-19 13:07:07
+category: category-test
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://ejiseok.github.io`,
   },
-  keywords: ["blog", "development", "asdf"],
   plugins: [],
 };
